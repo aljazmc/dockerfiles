965858fa5990f3b100e495dcf27d809250e3a6c8
aljazmc <mlinaric.aljaz@gmail.com>
2026-05-17 19:17:39 +0200

    feat: add corepack

---
+++ corepack/alpine/project.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+EXPECTED_COREPACK_VERSION="0.35.0"
+
+build() {
+
+docker build . -t aljazmc/corepack-alpine
+
+ACTUAL_COREPACK_VERSION=$(docker run aljazmc/corepack-alpine sh -c "corepack -v")
+docker tag aljazmc/corepack-alpine aljazmc/corepack-alpine:"${ACTUAL_COREPACK_VERSION}"
+docker tag aljazmc/corepack-alpine aljazmc/corepack-alpine:latest
+
+docker run aljazmc/corepack-alpine sh -c "printenv"
+docker image ls
+
+}
+
+clean() {
+
+docker system prune -af --volumes
+
+find . -mindepth 1 -maxdepth 1 \
+| sed "/Dockerfile/d;/docker-entrypoint.sh/d;/project.sh/d" \
+| xargs -I {} rm -rf {}
+
+}
+
+combo() {
+
+./project.sh clean && ./project.sh build
+
+}
+
+publish() {
+
+ACTUAL_COREPACK_VERSION=$(docker run aljazmc/corepack-alpine sh -c "corepack -v")
+
+[[ "${ACTUAL_COREPACK_VERSION}" == "${EXPECTED_COREPACK_VERSION}" ]] || \
+    { echo "ERROR: Unexpected corepack version"; exit; }
+
+docker push -a aljazmc/corepack-alpine
+
+}
+
+"$@"
---
+++ corepack/debian/project.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+EXPECTED_COREPACK_VERSION="0.35.0"
+
+build() {
+
+docker build . -t aljazmc/corepack-debian
+
+ACTUAL_COREPACK_VERSION=$(docker run aljazmc/corepack-debian sh -c "corepack -v")
+docker tag aljazmc/corepack-debian aljazmc/corepack-debian:"${ACTUAL_COREPACK_VERSION}"
+docker tag aljazmc/corepack-debian aljazmc/corepack-debian:latest
+
+docker run aljazmc/corepack-debian sh -c "printenv"
+docker image ls
+
+}
+
+clean() {
+
+docker system prune -af --volumes
+
+find . -mindepth 1 -maxdepth 1 \
+| sed "/Dockerfile/d;/docker-entrypoint.sh/d;/project.sh/d;/yarntest/d" \
+| xargs -I {} rm -rf {}
+
+}
+
+combo() {
+
+./project.sh clean && ./project.sh build
+
+}
+
+publish() {
+
+ACTUAL_COREPACK_VERSION=$(docker run aljazmc/corepack-debian sh -c "corepack -v")
+
+[[ "${ACTUAL_COREPACK_VERSION}" == "${EXPECTED_COREPACK_VERSION}" ]] || \
+    { echo "ERROR: Unexpected corepack version"; exit; }
+
+docker push -a aljazmc/corepack-debian
+
+}
+
+"$@"
