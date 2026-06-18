8824b4ef1ad2f385cf6337c313ac73a9dde4a575
aljazmc <mlinaric.aljaz@gmail.com>
2026-06-18 21:52:53 +0200

    feat: add community repository to suggested command

--- image-spinner/docker-entrypoint.sh
+++ image-spinner/docker-entrypoint.sh
@@ -17,10 +17,7 @@ profile_$PROFILENAME() {
                 cciss_vol_status lvm2 mdadm mkinitfs mtools nfs-utils
                 parted rsync sfdisk syslinux util-linux xfsprogs
                 dosfstools ntfs-3g
-                apache2
-	        mariadb mariadb-client
-	        php php85-curl php85-dom php85-exif php85-fileinfo php85-fpm php85-mysqlnd php85-mysqli php85-apache2 php85-openssl php85-xml php85-zip
-                "
+        "
         local _k _a
         for _k in \$kernel_flavors; do
                 apks="\$apks linux-\$_k"
@@ -37,6 +34,6 @@ chmod +x aports/scripts/mkimg.$PROFILENAME.sh
 echo ""
 echo "Configure aports/scripts/mkimg.$PROFILENAME.sh and run:"
 echo ""
-echo "docker run -it --rm --user aljazmc -v ./home:/home/aljazmc -w /home/aljazmc aljazmc/image-spinner:latest \"sh aports/scripts/mkimage.sh --tag edge --outdir iso --arch x86_64 --repository https://dl-cdn.alpinelinux.org/alpine/edge/main --profile $PROFILENAME\""
+echo "docker run -it --rm --user aljazmc -v ./home:/home/aljazmc -w /home/aljazmc aljazmc/image-spinner:latest \"sh aports/scripts/mkimage.sh --tag edge --outdir iso --arch x86_64 --repository https://dl-cdn.alpinelinux.org/alpine/edge/main --repository https://dl-cdn.alpinelinux.org/alpine/edge/community --profile $PROFILENAME\""
 echo ""
 echo "to build your very own Alpine ISO"
