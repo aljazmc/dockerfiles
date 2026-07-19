7cc79acc54851a8ada138e5a76a1c7d12f181687
aljazmc <mlinaric.aljaz@gmail.com>
2026-07-19 15:43:16 +0200

    chore: add update routine

--- corepack/debian/project.sh
+++ corepack/debian/project.sh
@@ -47,4 +47,19 @@ docker push -a aljazmc/corepack-debian
 
 }
 
+update() {
+
+LATEST_COREPACK_VERSION="$(docker run --rm aljazmc/corepack-alpine sh -c "NODE_OPTIONS='--no-warnings' npm --silent init --yes &> /dev/null && NODE_OPTIONS='--no-warnings' yarn add corepack --silent &&  NODE_OPTIONS='--no-warnings' yarn info corepack --silent | grep latest | sed \"s/latest\:\ \'//g;s/\'//g;s/\ //g\"")"
+# echo $LATEST_COREPACK_VERSION
+
+sed -i "s/^EXPECTED_COREPACK_VERSION=.*/EXPECTED_COREPACK_VERSION=\"$LATEST_COREPACK_VERSION\"/" project.sh
+
+LATEST_NODE_VERSION=$(wget -q https://nodejs.org/dist/ &>/dev/null && cat index.html | sed '/dist/d;/v[0-9]\./d;s/\/.*//;s/<a\ href="v//g;/^<$/d' | tail -n 1 && rm index.html)
+# echo $LATEST_NODE_VERSION
+
+sed -i "s/^\ \ \ \ NODE_VERSION=.*/    NODE_VERSION=\"$LATEST_NODE_VERSION\" \\\/" Dockerfile
+
+}
+
+
 "$@"
