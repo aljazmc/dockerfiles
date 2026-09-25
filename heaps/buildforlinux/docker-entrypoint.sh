364bc994b7bd8f7536d65774e0ac62532f505a8e
aljazmc <mlinaric.aljaz@gmail.com>
2026-09-25 22:34:31 +0200

    style: remove space

--- heaps/buildforlinux/docker-entrypoint.sh
+++ heaps/buildforlinux/docker-entrypoint.sh
@@ -37,7 +37,7 @@ if [ ! -f /home/"$USER"/src/Main.hx ]; then
 class Main extends hxd.App {
     override function init() {
         var tf = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
-        tf.text = "Hello Hashlink !";
+        tf.text = "Hello Hashlink!";
     }
     static function main() {
         new Main();
